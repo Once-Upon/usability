fc1d8fa2b08e429c05b9747297e330e838246aa5
ponyjackal <pony1125@outlook.com>
2024-02-01 06:37:14 -0800

    feat: add unit test for transactionAssetTransfersOldNFTs

--- src/transformer.spec.ts
+++ src/transformer.spec.ts
@@ -9,6 +9,8 @@ import { transform as transactionNetAssetTransfersTransform } from './transforme
 import { transform as transactionPartiesTransform } from './transformers/_common/transactionParties';
 import { transform as transactionSigHashTransform } from './transformers/_common/transactionSigHash';
 import { transform as transactionTimestampTransform } from './transformers/_common/transactionTimestamp';
+import { transform as transactionFeesTransform } from './transformers/ethereum/transactionFees';
+import { transform as transactionAssetTransfersOldNFTsTransform } from './transformers/ethereum/transactionAssetTransfersOldNFTs';
 
 describe('transformations', () => {
   it('TransactionAssetTransfers', () => {
@@ -185,7 +187,6 @@ describe('transformations', () => {
   it('transactionNetAssetTransfers', () => {
     const block = loadBlockFixture('ethereum', 16628971);
     const result = transactionNetAssetTransfersTransform(block);
-    console.log('result', result);
     const comboTx = result.find(
       (tx) =>
         tx.hash ===
@@ -357,6 +358,126 @@ describe('transformations', () => {
       expect(result[i].isoTimestamp).toBe(
         new Date(block.timestamp * 1000).toISOString(),
       );
+    }
+  });
+
+  it('transactionFees', () => {
+    const preLondonBlock = loadBlockFixture('ethereum', 12500000);
+    const preResult = transactionFeesTransform(preLondonBlock);
+    for (const tx of preResult) {
+      expect(tx.baseFeePerGas).toBe(preLondonBlock.baseFeePerGas);
+      expect(tx.burntFees).toBe('0');
+      expect(tx.minerFees).toBe(tx.transactionFee);
+    }
+
+    // and then test after
+    const postLondonBlock = loadBlockFixture('ethereum', 14573289);
+    const postResult = transactionFeesTransform(postLondonBlock);
+    for (const tx of postResult) {
+      expect(tx.baseFeePerGas).toBe(postLondonBlock.baseFeePerGas);
+    }
+
+    const postTx = postResult.find(
+      (tx) =>
+        tx.hash ===
+        '0x942e0ff43a8716ee324bef63c346a045839c44ccee15e49519726e6ba4ba3984',
+    );
+    expect(postTx).toBeDefined();
+    if (postTx) {
+      expect(postTx.burntFees).toBe('2557275674640775');
+      expect(postTx.minerFees).toBe('1693575757203250');
+    }
+  });
+
+  it('transactionAssetTransfersOldNFTs', () => {
+    // Special NFT transfers
+    /** CryptoKitties */
+    const cryptoKittiesBlock = loadBlockFixture('ethereum', 6082465);
+    const cryptoKittiesResult =
+      transactionAssetTransfersOldNFTsTransform(cryptoKittiesBlock);
+    const cryptoKittiesTx = cryptoKittiesResult.find(
+      (tx) =>
+        tx.hash ===
+        '0xcb6b23b24d3c0dd8d5ddaf8b9fae50c6742ff8bddf9fe18b4300b5e3ef73fea3',
+    );
+    expect(cryptoKittiesTx).toBeDefined();
+    if (cryptoKittiesTx) {
+      const cryptoKittiesTransfers = cryptoKittiesTx.assetTransfers;
+      expect(cryptoKittiesTransfers.length).toBe(1);
+      if ('tokenId' in cryptoKittiesTransfers[0]) {
+        expect(cryptoKittiesTransfers[0].tokenId).toBe('696398');
+      }
+      expect(cryptoKittiesTransfers[0].type).toBe('erc721');
+    }
+    /** CryptoPunks New */
+    const cryptoPunksNewBlock = loadBlockFixture('ethereum', 5774644);
+    const cryptoPunksNewResult =
+      transactionAssetTransfersOldNFTsTransform(cryptoPunksNewBlock);
+    const cryptoPunksNewTx = cryptoPunksNewResult.find(
+      (tx) =>
+        tx.hash ===
+        '0x0da4c50900119b47400d71a9dd3563571145e4e362b952c36a9e38c77f7d25bb',
+    );
+    expect(cryptoPunksNewTx).toBeDefined();
+    if (cryptoPunksNewTx) {
+      const cryptoPunksNewTransfers = cryptoPunksNewTx.assetTransfers;
+      expect(cryptoPunksNewTransfers[0].type).toBe('erc721');
+      if ('tokenId' in cryptoPunksNewTransfers[0]) {
+        expect(cryptoPunksNewTransfers[0].tokenId).toBe('89');
+      }
+    }
+    /** CryptoPunks Old */
+    const cryptoPunksOldBlock = loadBlockFixture('ethereum', 3862484);
+    const cryptoPunksOldResult =
+      transactionAssetTransfersOldNFTsTransform(cryptoPunksOldBlock);
+    const cryptoPunksOldTx = cryptoPunksOldResult.find(
+      (tx) =>
+        tx.hash ===
+        '0xff75a6739be926fe7328167011b5e2ac6a8883f55e76af70410520ef7b115901',
+    );
+    expect(cryptoPunksOldTx).toBeDefined();
+    if (cryptoPunksOldTx) {
+      const cryptoPunksOldTransfers = cryptoPunksOldTx.assetTransfers;
+      expect(cryptoPunksOldTransfers[0].type).toBe('erc721');
+      if ('tokenId' in cryptoPunksOldTransfers[0]) {
+        expect(cryptoPunksOldTransfers[0].tokenId).toBe('4851');
+      }
+    }
+    /** CryptoStriker */
+    const cryptoStrikersBlock = loadBlockFixture('ethereum', 15685187);
+    const cryptoStrikersResult =
+      transactionAssetTransfersOldNFTsTransform(cryptoStrikersBlock);
+    const cryptoStrikersTx = cryptoStrikersResult.find(
+      (tx) =>
+        tx.hash ===
+        '0xff760c32edcba188099fbc66ceb97ccd5917da91da7194e8db8f513c267d8d1a',
+    );
+    expect(cryptoStrikersTx).toBeDefined();
+    if (cryptoStrikersTx) {
+      const cryptoStrikersTransfers = cryptoStrikersTx.assetTransfers;
+      expect(cryptoStrikersTransfers.length).toBe(1);
+      expect(cryptoStrikersTransfers[0].type).toBe('erc721');
+      if ('tokenId' in cryptoStrikersTransfers[0]) {
+        expect(cryptoStrikersTransfers[0].tokenId).toBe('4896');
+      }
+    }
+    /** CryptoFighter */
+    const cryptoFightersBlock = loadBlockFixture('ethereum', 16751455);
+    const cryptoFightersResult =
+      transactionAssetTransfersOldNFTsTransform(cryptoFightersBlock);
+    const cryptoFightersTx = cryptoFightersResult.find(
+      (tx) =>
+        tx.hash ===
+        '0xd643017d3ae36bbe76bea7c3ed828ac5388a2698cb313957f8d3958e6bff548f',
+    );
+    expect(cryptoFightersTx).toBeDefined();
+    if (cryptoFightersTx) {
+      const cryptoFightersTransfers = cryptoFightersTx.assetTransfers;
+      expect(cryptoFightersTransfers.length).toBe(1);
+      expect(cryptoFightersTransfers[0].type).toBe('erc721');
+      if ('tokenId' in cryptoFightersTransfers[0]) {
+        expect(cryptoFightersTransfers[0].tokenId).toBe('3561');
+      }
     }
   });
 });
