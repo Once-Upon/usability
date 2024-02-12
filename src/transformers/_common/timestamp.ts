16f93f5c13299f2f677d50815eeced750714babc
ponyjackal <pony1125@outlook.com>
2024-02-12 11:52:55 -0800

    feat: remove isotimestamp

--- src/transformers/_common/timestamp.ts
+++ src/transformers/_common/timestamp.ts
@@ -1,11 +1,8 @@
 import type { RawBlock } from '../../types';
 
 export function transform(block: RawBlock): RawBlock {
-  const isoTimestamp = new Date(block.timestamp * 1000).toISOString();
-
   block.transactions = block.transactions.map((tx) => {
     tx.timestamp = block.timestamp;
-    tx.isoTimestamp = isoTimestamp;
     return tx;
   });
 
